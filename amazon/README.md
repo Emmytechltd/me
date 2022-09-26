f16c98a3089a99ffa2d90ea9a97117b0c876266e
Onu Emmanuel <82336786+Emmytechltd@users.noreply.github.com>
2022-09-26 04:51:40 +0100

    first commit

---
+++ amazon/README.md
@@ -0,0 +1,6 @@
+# MERN AMAZON
+# lessons
+1. Introduction
+2. Install tools
+3. create React app
+4. Create Git repository
---
+++ backend/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: "Hp Laptop",
+            slug: 'Core i5',
+            category: 'Computer',
+            image: '/images/dell1.jpg',
+            price: '#150,000',
+            countInStock: '20',
+            brand: 'Hp',
+            rating: '4.3',
+            numReviews: '08',
+            description: 'latest Generation'
+
+        },
+           {
+            name: "Hp Elitebook",
+            slug: 'Core i5',
+            category: 'Computer',
+            image: '/images/mac1.jpg',
+            price: '#225,000',
+            countInStock: '0',
+            brand: 'Hp',
+            rating: '4.3',
+            numReviews: '08',
+            description: 'latest Generation'
+
+        },
+           {
+            name: "Asus Laptop",
+            slug: 'Core i3',
+            category: 'Asus',
+            image: '/images/mac2.jpg',
+            price: '#120,000',
+            countInStock: '20',
+            brand: 'Hp',
+            rating: '4.3',
+            numReviews: '08',
+            description: 'latest Generation'
+
+        },
+           {
+            name: "MacBook Pro 💻",
+            slug: 'Core i3',
+            category: 'Apple',
+            image: '/images/mac3.jpg', //679px * 829px
+            price: '#120,000',
+            countInStock: '20',
+            brand: 'Hp',
+            rating: '4.3',
+            numReviews: '08',
+            description: 'Sleek and Durable'
+
+        },
+    ],
+};
+
+export default data;
